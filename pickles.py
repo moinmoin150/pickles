455a22d40e4948fef32184da60d0f96f2c1ae0a8
moinmoin150 <103155425+moinmoin150@users.noreply.github.com>
2022-07-21 12:30:21 -0500

    Update pickles.py

--- pickles.py
+++ pickles.py
@@ -11,7 +11,7 @@ def download_blob_into_memory(bucket_name, blob_name):
     storage_client = storage.Client(credentials=credentials)
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(blob_name)
-    contents = blob.download_as_string().decode("utf-8")
+    contents = blob.download_as_string()
     return pickle.loads(contents)
     
 df = pd.read_pickle(download_blob_into_memory("voluble_transcription", "pickles/tweets_df.pkl"))
