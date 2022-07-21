e4093a83a1be04ded341357cf1c22acfd3ac10f1
moinmoin150 <103155425+moinmoin150@users.noreply.github.com>
2022-07-21 12:20:56 -0500

    Create pickles.py

---
+++ pickles.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+from google.cloud import storage
+from google.oauth2 import service_account
+
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+
+def download_blob_into_memory(bucket_name, blob_name):
+    storage_client = storage.Client(credentials=credentials)
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    contents = blob.download_as_string().decode("utf-8")
+    return pickle.loads(contents)
+    
+df = pd.read_pickle(download_blob_into_memory("voluble_transcription", "pickles/tweets_df.pkl"))
+st.dataframe(df.head())
+
