3d6ba1b8e8fe945c671c3d0710f0cf20ddfcfc99
moinmoin150 <103155425+moinmoin150@users.noreply.github.com>
2022-07-21 13:15:18 -0500

    Update pickles.py

--- pickles.py
+++ pickles.py
@@ -12,7 +12,7 @@ def download_blob_into_memory(bucket_name, blob_name):
     storage_client = storage.Client(credentials=credentials)
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(blob_name)
-    contents = blob.download_as_string()
+    contents = blob.download_as_string().decode("utf-8")
     return contents
 
 # df = pickle.loads(download_blob_into_memory("voluble_transcription", "pickles/Replies to 1325516879033741319.csv"))
