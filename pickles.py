3dc69ee2247f4bf116a010787e0da9fc2441f903
moinmoin150 <103155425+moinmoin150@users.noreply.github.com>
2022-07-25 12:33:56 -0500

    Update pickles.py

--- pickles.py
+++ pickles.py
@@ -3,10 +3,17 @@ import pandas as pd
 from google.cloud import storage
 from google.oauth2 import service_account
 import pickle
+import os
 
-# export GOOGLE_APPLICATION_CREDENTIALS="google_crendentials.json"
 
-storage_client = storage.Client()
+json_str = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
+json_data = json.loads(json_str)
+json_data['private_key'] = json_data['private_key'].replace('\\n', '\n')
+
+credentials = service_account.Credentials.from_service_account_info(
+    json_data)
+
+storage_client = storage.Client(credentials=credentials)
 
 @st.experimental_memo
 def download_blob_into_memory(bucket_name, blob_name):
